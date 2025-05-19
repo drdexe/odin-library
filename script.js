e3bd469f010026bbe0bbd4f736efa88a1f1294f8
drdexe <104161856+drdexe@users.noreply.github.com>
2025-05-20 00:32:40 +0800

    Fix wrong deletion bug

--- script.js
+++ script.js
@@ -65,9 +65,9 @@ function displayLibrary() {
 
     const deleteCell = document.createElement("td");
     const deleteIcon = document.createElement("svg");
-    deleteIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>trash-can</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" /></svg>`;
+    deleteIcon.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>delete</title><path d="M9,3V4H4V6H5V19A2,2 0 0,0 7,21H17A2,2 0 0,0 19,19V6H20V4H15V3H9M9,8H11V17H9V8M13,8H15V17H13V8Z" /></svg>`;
     deleteIcon.addEventListener("click", () => {
-      removeBookFromLibrary(deleteIcon.parentNode.getAttribute("data-id"));
+      removeBookFromLibrary(deleteCell.parentNode.getAttribute("data-id"));
       displayLibrary();
     });
     deleteCell.appendChild(deleteIcon);
